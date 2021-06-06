f2ec4280b54078995401607272866fe43770581d
polyethylene <polyethylene@sjtu.edu.cn>
2021-06-06 16:53:40 +0800

    finish context.go

--- gee/router.go
+++ gee/router.go
@@ -1,6 +1,9 @@
 package gee
 
-import "log"
+import (
+	"log"
+	"net/http"
+)
 
 type router struct {
 	handlers map[string]HandlerFunc
@@ -20,5 +23,7 @@ func (r *router) handle(c *Context) {
 	key := c.Method + "-" + c.Path
 	if handler, ok := r.handlers[key]; ok {
 		handler(c)
+	} else {
+		c.String(http.StatusNotFound, "404 NOT FOUND: %s\n", c.Path)
 	}
 }
